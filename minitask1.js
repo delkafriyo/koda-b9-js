04364110719a1d3a1ac51df8ad8661462fc46d2f
delkafriyo <delkafriyo@gmail.com>
2026-07-15 11:43:03 +0700

    minitask

---
+++ minitask1.js
@@ -0,0 +1,29 @@
+// 1. Menampilkan nama
+let who = {
+    am : {
+        i : "Nando Delkafriyo"
+    }
+}
+
+console.log(who.am.i)
+
+// 2. Menampilkan "Hello World"
+
+let hello = {
+    world : "Hello World"
+}
+
+console.log(hello.world)
+
+// 3. Menampilkan string "Koda Tech Academy"
+
+let obj = {
+    str : [,[,,[,,,{koda : [{tech : {academy : "Koda Tech Academy"}}]}]]]
+}
+
+console.log(obj.str[1][2][3].koda[0].tech["academy"])
+
+// 4. Menampilkan Football
+let my = [{very: {best : [, {sport : {is : "Football"}}]}}]
+
+console.log(my[0].very["best"][1].sport.is)
