221066cd9a141c3d03c83c719b2589d2aacf7d46
delkafriyo <delkafriyo@gmail.com>
2026-07-15 14:30:08 +0700

    minitask

---
+++ minitask2.js
@@ -0,0 +1,17 @@
+let cost = 2000000;
+let totalCost = 0;
+let diskon = "0%";
+if (cost < 500000) {
+  diskon = "0%";
+  totalCost = cost;
+} else if (cost <= 1000000) {
+  diskon = "5%";
+  totalCost = cost - 0.05 * cost;
+} else {
+  diskon = "10%";
+  totalCost = cost - 0.1 * cost;
+}
+
+console.log(cost);
+console.log(diskon);
+console.log(totalCost);
